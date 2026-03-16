24a35bb3243d6502ec44d1d514550bb53567a7b5
Mschlindwein <schlindwein@hallowelt.com>
2026-03-16 10:50:33 +0100

    Add instance node to breadcrumbs
    ERM46946
    
    [5.x]
    
    Change-Id: Ic03c7fe6f70680a385379e8cf4d3c23f4654aa25

--- src/Hook/AddBootstrap.php
+++ src/Hook/AddBootstrap.php
@@ -3,15 +3,18 @@
 namespace BlueSpice\WikiFarm\Hook;
 
 use BlueSpice\WikiFarm\InstanceStore;
+use MediaWiki\Config\Config;
 use MediaWiki\Output\Hook\BeforePageDisplayHook;
 
 class AddBootstrap implements BeforePageDisplayHook {
 
 	/**
 	 * @param InstanceStore $instanceStore
+	 * @param Config $farmConfig
 	 */
 	public function __construct(
-		private readonly InstanceStore $instanceStore
+		private readonly InstanceStore $instanceStore,
+		private readonly Config $farmConfig
 	) {
 	}
 
@@ -20,12 +23,14 @@ class AddBootstrap implements BeforePageDisplayHook {
 	 */
 	public function onBeforePageDisplay( $out, $skin ): void {
 		$out->addModules( [ 'ext.bluespice.wikiFarm.bootstrap' ] );
-		$out->addModuleStyles( [ 'ext.bluespice.wikiFarm.megamenu' ] );
 		$instance = $this->instanceStore->getCurrentInstance();
 		if ( $instance && $instance->getMetadata()['instanceColor'] ) {
 			$out->getOutput()->addJsConfigVars(
 				'bsWikiFarmInstanceColor', $instance->getMetadata()['instanceColor']
 			);
+		}
+		if ( $this->farmConfig->get( 'useGlobalAccessControl' ) ) {
+			$out->addModuleStyles( [ 'ext.bluespice.wikiFarm.megamenu', 'ext.bluespice.wikiFarm.breadcrumbs' ] );
 		}
 	}
 }
