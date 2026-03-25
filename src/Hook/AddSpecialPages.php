ec6dabd27097f8ad6969dfe58542aa66840c236b
Mschlindwein <schlindwein@hallowelt.com>
2026-03-25 11:30:39 +0100

    Remove WikiTeams
    User group handling moved to BlueSpiceUserManager
    
    ERM46120
    
    [5.x]
    
    Change-Id: I5c96af4f9f7981f023e8aeb438f5632ecde731ec

--- src/Hook/AddSpecialPages.php
+++ src/Hook/AddSpecialPages.php
@@ -3,7 +3,6 @@
 namespace BlueSpice\WikiFarm\Hook;
 
 use BlueSpice\WikiFarm\Special\UserAccess;
-use BlueSpice\WikiFarm\Special\WikiTeams;
 use MediaWiki\Config\Config;
 use MediaWiki\SpecialPage\Hook\SpecialPage_initListHook;
 
@@ -18,10 +17,6 @@ class AddSpecialPages implements SpecialPage_initListHook {
 	/** @inheritDoc */
 	public function onSpecialPage_initList( &$list ) {
 		if ( $this->farmConfig->get( 'useGlobalAccessControl' ) ) {
-			$list['WikiTeams'] = [
-				'class' => WikiTeams::class,
-				'services' => [ 'BlueSpiceWikiFarm.TeamManager' ],
-			];
 			$list['UserAccess'] = [
 				'class' => UserAccess::class,
 				'services' => [ 'BlueSpiceWikiFarm._Config' ]
