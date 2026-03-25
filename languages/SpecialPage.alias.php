e021e74f39d961d6ff2ff9dea7059fb2711807ad
Mschlindwein <schlindwein@hallowelt.com>
2026-03-25 11:30:39 +0100

    Remove WikiTeams
    User group handling moved to BlueSpiceUserManager
    
    ERM46120
    
    [5.x]
    
    Change-Id: I5c96af4f9f7981f023e8aeb438f5632ecde731ec

--- languages/SpecialPage.alias.php
+++ languages/SpecialPage.alias.php
@@ -4,7 +4,6 @@ $specialPageAliases = [];
 /** English */
 $specialPageAliases['en'] = [
 	'FarmManagement' => [ 'Farm management', 'SpecialSimpleFarmer', 'SimpleFarmer', 'Farm manager' ],
-	'WikiTeams' => [ 'WikiTeams', 'Wiki teams', 'Teams' ],
 	'UserAccess' => [ 'UserAccess', 'User Access' ]
 ];
 
