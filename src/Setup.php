249fb563e2b2fc7db1e99b6b33fe1cfb0eb820df
Mschlindwein <schlindwein@hallowelt.com>
2026-09-11 15:48:59 +0200

    Improve colors in color palette on FarmManagement
    ERM50174
    
    [5.x]
    
    Change-Id: I4322efb8e7a089571c9b748c1d72c7a4c9740772
    (cherry picked from commit 97139a9117d0fe2e4dec8d85642be521ece1221e)

--- src/Setup.php
+++ src/Setup.php
@@ -15,31 +15,39 @@ class Setup {
 
 	private const INSTANCE_BADGE_COLORS = [
 		// Blues
-		[ 'background' => '#3F6F9F', 'lightText' => true ],
-		[ 'background' => '#5A9BD5', 'lightText' => true ],
-		[ 'background' => '#4F81BD', 'lightText' => true ],
+		[ 'background' => '#3C72A0', 'lightText' => true ],
+		[ 'background' => '#3364AD', 'lightText' => true ],
+		[ 'background' => '#4963B0', 'lightText' => true ],
+
+		// Purples / Pinks
+
+		[ 'background' => '#7E53A0', 'lightText' => true ],
+		[ 'background' => '#AD3B67', 'lightText' => true ],
+		[ 'background' => '#B82F63', 'lightText' => true ],
+
+		// Reds
+
+		[ 'background' => '#B7373F', 'lightText' => true ],
+
+		// Oranges / Yellow
+
+		[ 'background' => '#D96B1F', 'lightText' => false ],
+		[ 'background' => '#E59A2F', 'lightText' => false ],
+		[ 'background' => '#D6BD1A', 'lightText' => false ],
+
 		// Greens
-		[ 'background' => '#4C8C43', 'lightText' => true ],
-		[ 'background' => '#5FAE56', 'lightText' => true ],
-		[ 'background' => '#4F9A94', 'lightText' => true ],
-		[ 'background' => '#3E7F7A', 'lightText' => true ],
-		// Purples
-		[ 'background' => '#9A5D8C', 'lightText' => true ],
-		[ 'background' => '#B07AA1', 'lightText' => true ],
-		[ 'background' => '#7A5A78', 'lightText' => true ],
-		[ 'background' => '#9F86B8', 'lightText' => true ],
-		// Reds / Pinks
-		[ 'background' => '#C94C4E', 'lightText' => true ],
-		[ 'background' => '#E06C75', 'lightText' => true ],
-		[ 'background' => '#B85C82', 'lightText' => true ],
-		[ 'background' => '#D98DA4', 'lightText' => false ],
-		// Oranges
-		[ 'background' => '#D9791F', 'lightText' => true ],
-		[ 'background' => '#E89A4C', 'lightText' => true ],
-		[ 'background' => '#C96500', 'lightText' => true ],
-		[ 'background' => '#E5A96E', 'lightText' => false ],
-		// Teals
-		[ 'background' => '#5FA7A3', 'lightText' => true ],
+
+		[ 'background' => '#366F3C', 'lightText' => true ],
+		[ 'background' => '#1F705A', 'lightText' => true ],
+		[ 'background' => '#266D6A', 'lightText' => true ],
+
+		// Cyan
+
+		[ 'background' => '#246B82', 'lightText' => true ],
+
+		// Neutral
+
+		[ 'background' => '#5D666E', 'lightText' => true ],
 	];
 
 	/**
