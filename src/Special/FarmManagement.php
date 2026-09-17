ce29cb8d1fc2d4afb6cd530a65ef667aff3bbf54
Derick Alangi <alangiderick@gmail.com>
2026-09-17 12:05:34 +0300

    Special: Use `::setPageTitleMsg()` instead of `::setPageTitle()`
    In MediaWiki 1.45, the use of setPageTitle() with localization
    messages was no longer allowed. An equivalent method was introduced
    to handle this.
    
    This fixes an an error when trying to create a new wiki using the
    `Special:FarmManagement` feature. Without this patch on REL1_47,
    trying to create a wiki fails with the below error:
    
    ```
    ParameterTypeException: Bad value for parameter $name: must be a string
    ```
    
    Test plan
    =========
    1. On a given wiki, visit: `<wiki-url>/wiki/Special:Farm_management`
    2. Click on the "Create wiki" button
    3. Follow the next steps as required
    
    [galaxy]
    
    ERM50206
    
    Change-Id: I779772ae29298e69b86d73c743cfb6d4b3b6dfc3

--- src/Special/FarmManagement.php
+++ src/Special/FarmManagement.php
@@ -55,7 +55,7 @@ class FarmManagement extends SpecialPage {
 				);
 				return;
 			}
-			$this->getOutput()->setPageTitle( $this->msg( 'wikifarm-create-instance-title' ) );
+			$this->getOutput()->setPageTitleMsg( $this->msg( 'wikifarm-create-instance-title' ) );
 			$this->getOutput()->addBacklinkSubtitle( $this->getPageTitle() );
 			$this->getOutput()->addModules( [ 'ext.bluespice.wikiFarm.special.bootstrap' ] );
 
@@ -107,7 +107,7 @@ class FarmManagement extends SpecialPage {
 							);
 							return;
 						}
-						$this->getOutput()->setPageTitle(
+						$this->getOutput()->setPageTitleMsg(
 							$this->msg( 'wikifarm-create-instance-title-from-template', $template )
 						);
 						$createParams['template'] = $template;
